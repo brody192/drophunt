669002fdd4fe1b719f9a6092ac687f80e7cdc791
Nikita <nikita.voloboev@gmail.com>
2024-06-08 14:19:00 +0300

    .

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,16 +5,16 @@ const app = new Hono<{
 	Bindings: Bindings
 	Variables: Variables
 }>()
-
-app.use("*", ronin()).get("/", async (c) => {
-	const { get } = c.var.ronin
-	const [airdrops, ads, global] = await Promise.all([
-		get.airdrops(),
-		get.ads(),
-		get.global(),
-	])
-	return c.json({ airdrops, ads, global })
-})
+	.use("*", ronin())
+	.get("/", async (c) => {
+		const { get } = c.var.ronin
+		const [airdrops, ads, global] = await Promise.all([
+			get.airdrops(),
+			get.ads(),
+			get.global(),
+		])
+		return c.json({ airdrops, ads, global })
+	})
 
 export default app
 export type AppType = typeof app
