2aab57a4acf66ab2a274eb77dbc72f1842137146
Nikita <nikita.voloboev@gmail.com>
2024-06-09 14:30:07 +0300

    hacky but works

--- web/src/routes/HomeRoute.tsx
+++ web/src/routes/HomeRoute.tsx
@@ -1,13 +1,21 @@
 import { proxy } from "valtio"
 import { useProxy } from "valtio/utils"
-import { hc } from "hono/client"
-// TODO: should not be done this way (will fail in vercel deploy)
-import { AppType } from "../../../api/src/api"
+// TODO: should not be done this way (will fail in vercel deploy) (also failing locally too..)
+// import { AppType } from "../../../api/src/api"
+// import { hc } from "hono/client"
 import { useQuery } from "@tanstack/react-query"
+import { Ads, Airdrops, Global } from "@ronin/drophunt"
 
-export const HomeRouteState = proxy({})
+// TODO: breaking for some reason..
+// const client = hc<AppType>("https://drophunt.nikiv.workers.dev")
+// TODO: below should be inside queryFn of useQuery (but failing too..)
+// const response = await client.index.$get()
 
-const client = hc<AppType>("https://drophunt.nikiv.workers.dev")
+export const HomeRouteState = proxy({
+	ads: [] as Ads,
+	airdrops: [] as Airdrops,
+	global: {} as Global,
+})
 
 export default function HomeRoute() {
 	const local = useProxy(HomeRouteState)
@@ -15,15 +23,18 @@ export default function HomeRoute() {
 	const { error, data, isFetching } = useQuery({
 		queryKey: ["global"],
 		queryFn: async () => {
-			// const response = await client.index.$get()
 			const res = await fetch("https://drophunt.nikiv.workers.dev")
-			console.log(res, "res")
-			return await res.json()
+			const resJson = await res.json()
+			console.log(resJson, "res!!!")
+			local.ads = resJson.ads
+			local.airdrops = resJson.airdrops
+			local.global = resJson.global
+			return "success"
 		},
 	})
 	console.log(data, "data")
 
 	if (isFetching) return <div>Loading...</div>
 	if (error) return <div>Error: {JSON.stringify(error)}</div>
-	if (data) return <>{JSON.stringify(data)}</>
+	if (data) return <>{JSON.stringify(local.global)}</>
 }
