4233f88ec53d3d54a1a7593fe9868c6b1422c230
Nikita <nikita.voloboev@gmail.com>
2024-06-09 20:46:47 +0300

    .

--- app/src/routes/HomeRoute.tsx
+++ app/src/routes/HomeRoute.tsx
@@ -53,7 +53,7 @@ export default function HomeRoute() {
 		return (
 			<>
 				<div className="flex w-screen h-screen items-center justify-center">
-					<div className="w-[380px] h-[560px] bg-black overflow-auto rounded-[20px]">
+					<div className="bg-black overflow-auto rounded-[20px]">
 						<div className="py-[20px] px-[15px] text-white text-[14px] flex flex-col gap-[22px]">
 							<Nav />
 							<div className="w-full rounded-[20px] flex items-center justify-center bg-[#2e2e2e]">
